f60a8336f739ee03487c0454460a85ce3e7e8856
Zachary Duncan <71148283+Zach-Duncan@users.noreply.github.com>
2025-05-04 17:37:52 -0500

    Uodated pages further and edited backend features.

--- Models/topic.py
+++ Models/topic.py
@@ -42,3 +42,40 @@ class Topic:
         db.commit()
 
         return topic_id
+
+    @staticmethod
+    def get_by_id(topic_id):
+        db = DBConnection.get_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM topics WHERE id = %s", (topic_id,))
+        return cursor.fetchone()
+
+    @staticmethod
+    def is_user_subscribed(user_id, topic_id):
+        db = DBConnection.get_connection()
+        cursor = db.cursor()
+        cursor.execute("SELECT 1 FROM subscriptions WHERE user_id = %s AND topic_id = %s", (user_id, topic_id))
+        return cursor.fetchone() is not None
+
+    @staticmethod
+    def get_subscribers(topic_id):
+        db = DBConnection.get_connection()
+        cursor = db.cursor(dictionary=True)
+        cursor.execute("""
+            SELECT u.id, u.username
+            FROM users u
+            JOIN subscriptions s ON u.id = s.user_id
+            WHERE s.topic_id = %s
+        """, (topic_id,))
+        return cursor.fetchall()
+
+    @staticmethod
+    def increment_access_count(topic_id):
+        db = DBConnection.get_connection()
+        cursor = db.cursor()
+        cursor.execute("""
+            UPDATE topics 
+            SET access_count = access_count + 1 
+            WHERE id = %s
+        """, (topic_id,))
+        db.commit()
